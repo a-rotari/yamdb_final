ff8dc0c8088cd8a46948b3bac7baa3fd5f3a7097
Andrei Rotar <andrei.rotari.work@gmail.com>
2021-07-06 13:08:06 +0300

    Corrected a typo to pass flake8.

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'redoc'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'redoc'), ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
